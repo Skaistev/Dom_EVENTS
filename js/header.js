46f35abc9f36df1322ad5fc72bda13c6a0432eca
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-02-27 15:34:05 +0200

    pap

--- js/header.js
+++ js/header.js
@@ -7,11 +7,11 @@ function header() {
 
 
     const HTML = `
-        <a href="${href}">Home</a>
-        <a href="${href}single-button-click">Single button click</a>
-        <a href="${href}counter/">Counter</a>
-        <a href="${href}basketball/">Basketball</a>
-        <a href="${href}virykle/">Virykle</a>
+        <a href="${href}Dom_EVENTS">Home</a>
+        <a href="${href}Dom_EVENTS/single-button-click">Single button click</a>
+        <a href="${href}Dom_EVENTS/counter/">Counter</a>
+        <a href="${href}Dom_EVENTS/basketball/">Basketball</a>
+        <a href="${href}Dom_EVENTS/virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
