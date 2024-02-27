4b30eefda0a4e62bfdcdbd72fe387908c1ad0531
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-02-27 14:16:08 +0200

    PAPILDYTAS

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === 'localhost' ? '/' : './';
+    const href = location.hostname === 'localhost' ? '/' : '././';
     const path = location.pathname;
     console.log(path)
 
     const HTML = `
-        <a href="${href}">Home</a>
+        <a href="${href}>Home</a>
         <a href="${href}single-button-click">Single button click</a>
         <a href="${href}counter/">Counter</a>
         <a href="${href}basketball/">Basketball</a>
