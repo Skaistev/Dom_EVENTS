9d12fa3bcaf8d1d316b63ff6d6dfc6ecd45bb21d
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-02-27 15:48:39 +0200

    aha

--- js/header.js
+++ js/header.js
@@ -3,15 +3,15 @@ function header() {
     const titleDom = document.querySelector('h1');
     console.log(titleDom.innerText)
     console.log(location.pathname)
-    const href = location.hostname === 'localhost' ? '/' : location.pathname.includes(titleDom.innerText)? "./": "../";
-
+    const href = location.hostname === 'localhost' ? '/' : './'
+    // location.pathname.includes(titleDom.innerText)? "./": "../";
 
     const HTML = `
-        <a href="${href}Dom_EVENTS">Home</a>
-        <a href="${href}Dom_EVENTS/single-button-click">Single button click</a>
-        <a href="${href}Dom_EVENTS/counter/">Counter</a>
-        <a href="${href}Dom_EVENTS/basketball/">Basketball</a>
-        <a href="${href}Dom_EVENTS/virykle/">Virykle</a>
+        <a href="${href}">Home</a>
+        <a href="${href}single-button-click/">Single button click</a>
+        <a href="${href}counter/">Counter</a>
+        <a href="${href}basketball/">Basketball</a>
+        <a href="${href}virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
